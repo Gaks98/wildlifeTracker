cdc96bb254da48c34dfdedaa207d79d7752af8c5
Gaks98 <yvonnegax98@gmail.com>
2019-09-13 14:50:28 +0300

    getLocation and getRangerName instantiates correctly

--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -6,10 +6,27 @@ public class SightingTest {
 
     @Test
     public void Sighting_instantiatesCorrectly_true() {
-        Sighting testSighting = new Sighting("Zone A", "Henry");
+        Sighting testSighting = setupNewSighting();
         assertEquals(true, testSighting instanceof Sighting);
     }
 
-    
+    @Test
+    public void getLocation_sightingInstantiatesWithLocation_ZoneA() {
+        Sighting testSighting = setupNewSighting();
+        assertEquals("Zone A", testSighting.getLocation());
+    }
 
+    @Test
+    public void getRangerName_sightingInstantiatesWithRangerName_String() {
+        Sighting testSighting = setupNewSighting();
+        assertEquals("Henry", testSighting.getRangerName());
+    }
+
+    //helper method
+    public Sighting setupNewSighting(){
+        return new Sighting("Zone A", "Henry");
+    }
+    public Sighting setupOtherSighting(){
+        return new Sighting("Zone B", "Alex");
+    }
 }
