b0c8c1240ba06d222293dcdebb8cef6099576b80
Gaks98 <yvonnegax98@gmail.com>
2019-09-13 14:39:08 +0300

    sighting instantes correctly

--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -1,5 +1,15 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SightingTest {
+
+    @Test
+    public void Sighting_instantiatesCorrectly_true() {
+        Sighting testSighting = new Sighting("Zone A", "Henry");
+        assertEquals(true, testSighting instanceof Sighting);
+    }
+
+    
 
 }
