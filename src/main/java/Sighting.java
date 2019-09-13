d4335704e8790288ec936545fe642f25dec633f7
Gaks98 <yvonnegax98@gmail.com>
2019-09-13 16:20:46 +0300

    added the save() method in Sighting.java

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -1,3 +1,5 @@
+import org.sql2o.Connection;
+
 public class Sighting {
 
         private String location;
@@ -16,6 +18,16 @@ public class Sighting {
             Sighting newSighting = (Sighting) otherSighting;
             return this.getLocation().equals(newSighting.getLocation()) &&
                     this.getRangerName().equals(newSighting.getRangerName());
+        }
+    }
+
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO sightings (location, rangerName) VALUES (:location, :rangerName)";
+            con.createQuery(sql)
+                    .addParameter("location", this.location)
+                    .addParameter("rangerName", this.rangerName)
+                    .executeUpdate();
         }
     }
 
