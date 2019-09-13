5dbf786f50dc11ffedf96e974b5e9d0b7a2976ad
Gaks98 <yvonnegax98@gmail.com>
2019-09-13 14:34:06 +0300

    created simple POJO structure

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -1,2 +1,18 @@
 public class Sighting {
+
+        private String location;
+        private String rangerName;
+
+        public Sighting(String location, String rangerName) {
+            this.location = location;
+            this.rangerName = rangerName;
+        }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getRangerName() {
+        return rangerName;
+    }
 }
