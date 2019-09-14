9f425792bc2fea018da5ed4cc6db9db028b4f43c
Gaks98 <yvonnegax98@gmail.com>
2019-09-14 14:32:14 +0300

    added a name property to Animal.java together with it's getter and a constructor with 2 parameters

---
+++ src/main/java/Animal.java
@@ -0,0 +1,11 @@
+public class Animal {
+    private String name;
+
+    public Animal(String name, int sightingId) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
