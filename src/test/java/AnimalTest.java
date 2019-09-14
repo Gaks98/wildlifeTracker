d51a14c9b4cab81baaa96de4304c21880647480f
Gaks98 <yvonnegax98@gmail.com>
2019-09-14 14:45:41 +0300

    added test for overiding equals

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -9,19 +9,25 @@ public class AnimalTest {
      @Rule
         public DatabaseRule database = new DatabaseRule();
 
-        @Test
-        public void Animal_instantiatesCorrectly_true() {
-            assertEquals(true, testAnimal instanceof Animal);
-        }
+     @Test
+     public void Animal_instantiatesCorrectly_true() {
+        assertEquals(true, testAnimal instanceof Animal);
+     }
 
-        @Test
-        public void Animal_instantiatesWithName_String() {
+     @Test
+     public void Animal_instantiatesWithName_String() {
         assertEquals("Wolf", testAnimal.getName());
-        }
+     }
 
     @Test
     public void Animal_instantiatesWithSightingId_int() {
         assertEquals(1, testAnimal.getSightingId());
+    }
+
+    @Test
+    public void equals_returnsTrueIfNameAndPersonIdAreSame_true() {
+        Animal anotherAnimal = new Animal("Bubbles", 1);
+        assertTrue(testAnimal.equals(anotherAnimal));
     }
 
 }
