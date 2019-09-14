2671b4efa9ba3c494d07ef14729a5539ad976251
Gaks98 <yvonnegax98@gmail.com>
2019-09-14 14:35:41 +0300

    Animal instantiates correctly and also the getName() instantiates with animal

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,5 +1,22 @@
+import org.junit.Rule;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class AnimalTest {
+    Animal testAnimal = new Animal("Wolf", 1);
+
+     @Rule
+        public DatabaseRule database = new DatabaseRule();
+
+        @Test
+        public void Animal_instantiatesCorrectly_true() {
+            assertEquals(true, testAnimal instanceof Animal);
+        }
+
+        @Test
+        public void Animal_instantiatesWithName_String() {
+        assertEquals("Wolf", testAnimal.getName());
+        }
 
 }
